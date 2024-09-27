25052e2faad6b93d5f0f8dcb2de712621613966b
booster <lopez.hermelindo@outlook.com>
2024-09-27 11:11:35 -0700

    Update page.tsx

--- app/materials/[id]/page.tsx
+++ app/materials/[id]/page.tsx
@@ -2,7 +2,7 @@ export default function ProductPage({ params }: { params: { id: string } }) {
     console.log(params.id)
     return (
         <div >
-            <h1>Material Details</h1>
+            <h1>Material Details {params.id}</h1>
         </div>
     );
 }
