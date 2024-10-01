da521b00e1517f172043ff28a628a04dd1d79528
booster <lopez.hermelindo@outlook.com>
2024-09-30 23:15:05 -0700

    update

--- api/src/app.ts
+++ api/src/app.ts
@@ -93,7 +93,7 @@ dotenv.config();
 
 // const env = require("./util/validateEnv");
 
-const port = process.env.PORT || 3000;
+const port = 3001;
 
 app.listen(port, () => {
   console.log("Supabase connected");
